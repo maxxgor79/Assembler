64612187fd5d402346e401a375030271b8d9c847
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2023-11-28 02:32:21 +0300

    Added configuration

--- other/conv.bat
+++ other/conv.bat
@@ -1,1 +1,1 @@
-native-image.exe -jar %1% z80asm --no-fallback -H:IncludeResourceBundles=i18n.Messages -H:IncludeResources=template/z80/[a-zA-Z]+.op$ -H:IncludeResources=settings.properties$ -H:IncludeResources=settings.properties$ -H:IncludeResources=log4j.properties$
+C:\Users\Maxim\works\graalvm-jdk-21.0.1.12.1\bin\native-image -jar %1 z80asm --no-fallback -H:IncludeResourceBundles=i18n.Messages -H:IncludeResources=template/z80/[a-zA-Z]+.op$ -H:IncludeResources=log4j.properties$
---
+++ other/conv2.bat
@@ -0,0 +1,1 @@
+C:\Users\Maxim\works\graalvm-jdk-21.0.1.12.1\bin\java -agentlib:native-image-agent=config-output-dir=conf -jar %1
