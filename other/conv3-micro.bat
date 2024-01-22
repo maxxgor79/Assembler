0b0d7cf182a8c1ec43e6fcbd006e3c3d13d24971
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2024-01-22 15:46:25 +0300

    Updated converter to native files

---
+++ other/conv3-micro.bat
@@ -0,0 +1,1 @@
+C:\Users\Maxim\works\graalvm-jdk-21.0.1.12.1\bin\native-image -jar %1 mcsasm -H:ConfigurationFileDirectories=conf --no-fallback -H:IncludeResourceBundles=i18n.Messages,i18n.MicroshaMessages -H:IncludeResources=template/z80/[a-zA-Z]+.op$ -H:IncludeResources=log4j.properties$ -H:IncludeResources=settings.properties$
