b2ba383cfa6f3ce02ee1a343b588eef2b9d2baee
Maxim_Gorin <magorin@vtb.ru>
2024-01-26 20:48:49 +0300

    Added Wav reader, tests

---
+++ core/src/main/java/ru/assembler/io/wav/WavInputStream.java
@@ -0,0 +1,117 @@
+package ru.assembler.io.wav;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import ru.assembler.io.LEDataInputStream;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+public class WavInputStream extends InputStream {
+    protected static final int PCM_FORMAT = 1;
+
+    @Getter
+    protected short bps;
+
+    @Getter
+    private int sampleRate;
+
+    @Getter
+    private int byteRate;
+
+    @Getter
+    private short blockAlign;
+
+    @Getter
+    private short numChannels;
+
+    @Getter
+    private int dataSize;
+
+    @Getter
+    private int estimatedDataSize;
+
+    private LEDataInputStream leDis;
+
+    public WavInputStream(@NonNull InputStream is) throws IOException {
+        this.leDis = new LEDataInputStream(is);
+        readHeader();
+    }
+
+    private void readHeader() throws IOException {
+        final byte[] riff = new byte[4];
+        leDis.read(riff);
+        if (!"RIFF".equals(new String(riff))) {
+            throw new IOException("Bad header ID:" + new String(riff));
+        }
+        dataSize = leDis.readInt() - 36;
+        final byte[] wave = new byte[4];
+        leDis.read(wave);
+        if (!"WAVE".equals(new String(wave))) {
+            throw new IOException("Bad chunk header ID:" + new String(wave));
+        }
+        final byte[] fmt = new byte[4];
+        leDis.read(fmt);
+        if (!"fmt ".equals(new String(fmt))) {
+            throw new IOException("Bad chunk header ID:" + new String(fmt));
+        }
+        final int chunkSize = leDis.readInt();
+        if (chunkSize != 16) {
+            throw new IOException("Bad chunk size: " + chunkSize);
+        }
+        final short format = leDis.readShort();
+        if (format != PCM_FORMAT) {
+            throw new IOException("Unsupported format:" + format);
+        }
+        this.numChannels = leDis.readShort();
+        this.sampleRate = leDis.readInt();
+        this.byteRate = leDis.readInt();
+        this.blockAlign = leDis.readShort();
+        this.bps = leDis.readShort();
+        final byte[] data = new byte[4];
+        leDis.read(data);
+        if (!"data".equals(new String(data))) {
+            throw new IOException("Bad chunk header ID:" + new String(data));
+        }
+        dataSize = estimatedDataSize = leDis.readInt();
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (estimatedDataSize > 0) {
+            estimatedDataSize--;
+            return leDis.read();
+        }
+        return -1;
+    }
+
+    public int readSample() throws IOException {
+        int ch1, ch2;
+        switch (bps) {
+            case 8:
+                return read();
+            case 16:
+                ch1 = read();
+                ch2 = read();
+                if ((ch1 | ch2) < 0)
+                    throw new EOFException();
+                return (ch2 << 8) + (ch1 << 0);
+            default:
+                throw new IOException("Unsupported format");
+        }
+    }
+
+    public int[] readSamples() throws IOException {
+        switch (numChannels) {
+            case 1:
+                return new int[]{readSample()};
+            case 2:
+                return new int[]{readSample(), readSample()};
+            default:
+                throw new IOException("Unsupported channel number");
+        }
+    }
+}
---
+++ core/src/test/java/ru/assembler/core/TestIO.java
@@ -0,0 +1,47 @@
+package ru.assembler.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.assembler.io.wav.WavInputStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+public class TestIO {
+    @Test
+    public void testWav1() throws IOException {
+        try (FileInputStream fis = new FileInputStream("src/test/java/ru/assembler/core/8mono.wav")) {
+            WavInputStream wis = new WavInputStream(fis);
+            Assertions.assertEquals(wis.getBps(), 8);
+            Assertions.assertEquals(wis.getNumChannels(), 1);
+            Assertions.assertEquals(wis.getSampleRate(),8000);
+            Assertions.assertEquals(wis.getDataSize(), 8000);
+            int b, readBytes = 0;
+            while((b = wis.read()) != -1) {
+                readBytes++;
+            }
+            Assertions.assertEquals(readBytes, 8000);
+        }
+    }
+
+    @Test
+    public void testWav2() throws IOException {
+        try (FileInputStream fis = new FileInputStream("src/test/java/ru/assembler/core/16mono.wav")) {
+            WavInputStream wis = new WavInputStream(fis);
+            Assertions.assertEquals(wis.getBps(), 16);
+            Assertions.assertEquals(wis.getNumChannels(), 1);
+            Assertions.assertEquals(wis.getSampleRate(),11025);
+            Assertions.assertEquals(wis.getDataSize(), 22050);
+            int b, readBytes = 0;
+            while((b = wis.read()) != -1) {
+                readBytes++;
+            }
+            Assertions.assertEquals(readBytes, 22050);
+        }
+    }
+}
