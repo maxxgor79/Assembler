54a55490afb532eee4db8019ef68e0a92d3fa6c4
maxim <maxxgor79@yahoo.com>
2024-03-10 00:52:08 +0300

    Test is modified

--- editor/src/main/java/ru/retro/assembler/editor/core/settings/Settings.java
+++ editor/src/main/java/ru/retro/assembler/editor/core/settings/Settings.java
@@ -1,16 +1,9 @@
 package ru.retro.assembler.editor.core.settings;
 
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import ru.retro.assembler.editor.core.access.AnnotationProcessor;
 import ru.retro.assembler.editor.core.access.Property;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
--- editor/src/test/java/ru/retro/assembler/editor/TestSettings.java
+++ editor/src/test/java/ru/retro/assembler/editor/TestSettings.java
@@ -1,29 +1,21 @@
 package ru.retro.assembler.editor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.configuration.ConfigurationException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.retro.assembler.editor.core.settings.AppSettings;
-import ru.retro.assembler.editor.core.settings.Settings;
-
-import java.io.ByteArrayOutputStream;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 public class TestSettings {
     @Test
-    public void test1Save() throws ConfigurationException {
+    public void test1Save() {
         final AppSettings settings = new AppSettings();
         settings.setMainFramePosX(1);
         settings.setMainFramePosY(2);
         settings.setMainFrameWidth(3);
         settings.setMainFrameHeight(4);
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        settings.save(baos);
-        String s = new String(baos.toByteArray());
-    }
+     }
 
 }
