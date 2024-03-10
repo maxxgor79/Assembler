a675f90b98e4ea46c935e8f8050bbca1a426f6b4
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2024-03-10 19:41:04 +0300

    Added installer for windows

---
+++ editor/installer/windows/makeinst.bat
@@ -0,0 +1,1 @@
+jpackage -i app --name RetroIDE --main-jar editor-1.0-jar-with-dependencies.jar --main-class ru.retro.assembler.editor.core.Editor --type app-image --verbose --icon res\microchip.png --dest dest --java-options -Dz80asm.embedded=true
