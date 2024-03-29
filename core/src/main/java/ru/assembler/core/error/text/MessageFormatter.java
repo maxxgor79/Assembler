7ca4d718541bca84b3344654ea5d80349e9120bb
Maxim_Gorin1 <mgorin@dev.vtb.ru>
2024-03-30 01:16:37 +0300

    Error format is corrected

--- core/src/main/java/ru/assembler/core/error/text/MessageFormatter.java
+++ core/src/main/java/ru/assembler/core/error/text/MessageFormatter.java
@@ -14,7 +14,7 @@ public final class MessageFormatter {
 
     public static String generateErrorText(FileDescriptor fd, int lineNumber, String text, String... args) {
         StringBuilder sb = new StringBuilder();
-        sb.append("[").append(MessageList.getMessage(MessageList.ERROR)).append("]").append("%s: [%s] %s");
+        sb.append("[").append(MessageList.getMessage(MessageList.ERROR)).append("] ").append("%s [%s]: %s");
         String message = String.format(sb.toString(), fd == null ? "" : fd.getDisplay()
                 , Integer.valueOf(lineNumber), String.format(text, args));
         return message;
@@ -22,7 +22,7 @@ public final class MessageFormatter {
 
     public static String generateWarningText(FileDescriptor fd, int lineNumber, String text, String... args) {
         final StringBuilder sb = new StringBuilder();
-        sb.append("[").append(MessageList.getMessage(MessageList.WARNING)).append("]").append("%s: [%s] %s");
+        sb.append("[").append(MessageList.getMessage(MessageList.WARNING)).append("]").append("%s [%s]: %s");
         final String message = String.format(sb.toString(), fd.getDisplay() == null ? "" :
                         fd.getDisplay(), Integer.valueOf(lineNumber), String.format(text, args));
         return message;
